5bdb2641244a9a7107e0510cad992328dd1d0c9d
J. Asturiano <48779823+jasturiano@users.noreply.github.com>
2022-02-06 16:30:04 -0600

    Update dbt_dag.py

--- airflow/dags/dbt_dag.py
+++ airflow/dags/dbt_dag.py
@@ -3,23 +3,20 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
-# We're hardcoding this value here for the purpose of the demo, but in a production environment this
-# would probably come from a config file and/or environment variables!
+#Better to use a Environment Variable here
 DBT_PROJECT_DIR = '/usr/local/airflow/dbt'
 
 
 dag = DAG(
-    "dbt_basic_dag",
-    start_date=datetime(2020, 12, 23),
-    default_args={"owner": "astronomer", "email_on_failure": False},
-    description="A sample Airflow DAG to invoke dbt runs using a BashOperator",
+    "dbt_dag",
+    start_date=datetime(2021, 01, 01),
+    description="Invoke dbt using the BashOperator",
     schedule_interval=None,
     catchup=False,
 )
 
 with dag:
-    # This task loads the CSV files from dbt/data into the local postgres database for the purpose of this demo.
-    # In practice, we'd usually expect the data to have already been loaded to the database.
+    #In case we need to load csv or any raw files into DB, not recommended for big files
     dbt_seed = BashOperator(
         task_id="dbt_seed",
         bash_command=f"dbt seed --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}"
